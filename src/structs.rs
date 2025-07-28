e76aad0643f8c1511f562c88eac250c24f59e71e
junkicide <ajinkyak@protonmail.com>
2025-07-28 06:12:03 +0200

    feat: add timestamp

--- src/structs.rs
+++ src/structs.rs
@@ -1,3 +1,5 @@
+use chrono::{DateTime, Utc};
+
 use alloy::primitives::FixedBytes;
 use alloy_rpc_types::TransactionReceipt;
 use serde_derive::Serialize;
@@ -6,6 +8,7 @@ pub(crate) type Opcode = String;
 
 #[derive(Serialize)]
 pub struct GasKillerReport {
+    pub time: DateTime<Utc>,
     pub tx_hash: FixedBytes<32>,
     pub block_hash: FixedBytes<32>,
     pub gas_used: u64,
@@ -21,8 +24,9 @@ pub struct GasKillerReport {
 }
 
 impl GasKillerReport {
-    pub fn report_error(receipt: &TransactionReceipt, e: &anyhow::Error) -> Self {
+    pub fn report_error(time: DateTime<Utc>, receipt: &TransactionReceipt, e: &anyhow::Error) -> Self {
         GasKillerReport {
+            time,
             tx_hash: receipt.transaction_hash,
             block_hash: receipt.block_hash.unwrap_or_else(|| {
                 panic!(
@@ -42,8 +46,9 @@ impl GasKillerReport {
             error_log: Some(format!("{e:?}")),
         }
     }
-    pub fn from(receipt: &TransactionReceipt, details: ReportDetails) -> Self {
+    pub fn from(time: DateTime<Utc>, receipt: &TransactionReceipt, details: ReportDetails) -> Self {
         GasKillerReport {
+            time,
             tx_hash: receipt.transaction_hash,
             block_hash: receipt.block_hash.unwrap_or_else(|| {
                 panic!(
