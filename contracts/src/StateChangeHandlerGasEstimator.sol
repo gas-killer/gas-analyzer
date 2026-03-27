abbb84fd2a95153e19ed14ac8656f68251bc04fa
rubydusa <rubydusa@protonmail.com>
2026-03-27 14:12:36 +0300

    feat: REENTRANCY_CHECK_SLOT

--- contracts/src/StateChangeHandlerGasEstimator.sol
+++ contracts/src/StateChangeHandlerGasEstimator.sol
@@ -22,6 +22,11 @@ contract StateChangeHandlerGasEstimator {
     bytes32 private constant IMPL_SLOT =
         0x96d8ea8ab34935626cad61e29511d513907fd7c186676bae1b82974066723cbf;
 
+    /// @dev Isolated storage slot for tracking if fallback was called during `runStateUpdatesCall`
+    /// keccak256("gas.estimator.reentrancy") - 1
+    bytes32 private constant REENTRANCY_CHECK_SLOT =
+        0x242ffd5c5678a014b50279a5c080b8776eeb4383dff0782c0b229c029f801303;
+
     constructor(address _implementation) {
         assembly {
             sstore(IMPL_SLOT, _implementation)
@@ -30,11 +35,32 @@ contract StateChangeHandlerGasEstimator {
 
     function runStateUpdatesCall(StateUpdateType[] memory types, bytes[] memory args) external {
         StateChangeHandlerLib._runStateUpdates(types, args);
+
+        // cold sload: 2000
+        // hot sload: 100
+        // if gas diff is less than 2000 than guaranteed it was a hot slot
+        uint256 gasBefore = gas();
+        assembly {
+            sload(REENTRANCY_CHECK_SLOT)
+        }
+        uint256 gasAfter = gas();
+        if (gasBefore - gasAfter < 2000) {
+            assembly {
+                sstore(REENTRANCY_CHECK_SLOT, 1)
+            }
+        }
+    }
+
+    function fallbackWasCalled() external view returns (bool result) {
+        assembly {
+            result := sload(REENTRANCY_CHECK_SLOT)
+        }
     }
 
     /// @dev Forward any unknown selector to the original implementation via DELEGATECALL.
     fallback() external payable {
         assembly {
+            sload(REENTRANCY_CHECK_SLOT)  // load slot so in the end of `runStateUpdatesCall`
             let impl := sload(IMPL_SLOT)
             calldatacopy(0, 0, calldatasize())
             let success := delegatecall(gas(), impl, 0, calldatasize(), 0, 0)
